12cedcd6ecc95090eb6b20d463757496bada39c1
Tuzilow <1176281967@qq.com>
2020-08-08 18:30:42 +0800

    房间详情

--- pages/room-detail/room-detail.js
+++ pages/room-detail/room-detail.js
@@ -1,18 +1,44 @@
 // pages/room-detail/room-detail.js
+const app = getApp();
+
 Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    room: {},
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     const { roomid } = options;
-    console.log(roomid);
+    this.getRoom(roomid);
   },
 
+  getRoom: function (id) {
+    wx.request({
+      url: `${app.globalData.root_url}rooms/${id}`,
+      header: {
+        Authorization: `${wx.getStorageSync('token')}`,
+      },
+      success: (res) => {
+        const { code, data } = res.data;
+        if (code === 0) {
+          data['img_url'].forEach((item, index) => {
+            data['img_url'][index] = app.globalData['root_url'] + item;
+            data['room_info'] = JSON.parse(data['room_info']);
+            data['computer_info'] = JSON.parse(data['computer_info']);
+          });
+
+          this.setData({
+            room: data,
+          });
+        }
+      },
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
