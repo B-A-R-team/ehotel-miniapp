1f9f3fdf94d3118da05c1dfac895c7a767925492
Tuzilow <1176281967@qq.com>
2020-11-28 17:30:20 +0800

    修改为用户每次都要登录

--- app.js
+++ app.js
@@ -4,25 +4,26 @@ const ROOTURL = 'https://www.barteam.cn:1239/';
 
 App({
   onLaunch: function () {
-    wx.checkSession({
-      success: () => {
-        console.log('登陆未过期');
-        // 登陆未过期，取出数据
-        const userId = wx.getStorageSync('userId') || null;
-        const nickName = wx.getStorageSync('nickname') || null;
-        const avatarUrl = wx.getStorageSync('avatar_url') || null;
-        const intergal = wx.getStorageSync('intergal') || 0;
-        const paid_balance = wx.getStorageSync('paid_balance') || 0;
+    wx.clearStorageSync();
+    // wx.checkSession({
+    //   success: () => {
+    //     console.log('登陆未过期');
+    //     // 登陆未过期，取出数据
+    //     const userId = wx.getStorageSync('userId') || null;
+    //     const nickName = wx.getStorageSync('nickname') || null;
+    //     const avatarUrl = wx.getStorageSync('avatar_url') || null;
+    //     const intergal = wx.getStorageSync('intergal') || 0;
+    //     const paid_balance = wx.getStorageSync('paid_balance') || 0;
 
-        this.globalData.userInfo = { userId, nickName, avatarUrl };
-        this.globalData['intergal'] = intergal;
-        this.globalData['paid_balance'] = paid_balance;
-      },
-      fail() {
-        console.log('登陆已过期');
-        wx.clearStorageSync();
-      },
-    });
+    //     this.globalData.userInfo = { userId, nickName, avatarUrl };
+    //     this.globalData['intergal'] = intergal;
+    //     this.globalData['paid_balance'] = paid_balance;
+    //   },
+    //   fail() {
+    //     console.log('登陆已过期');
+    //     wx.clearStorageSync();
+    //   },
+    // });
   },
   globalData: {
     userInfo: null,
